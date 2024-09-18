00da507fb71ee9e7b409ac693d84355c45e58aa8
zharfan-a <zharfan@walturn.com>
2024-09-18 14:39:33 +0700

    Add src/quiz.py

---
+++ src/quiz.py
@@ -0,0 +1,26 @@
+
+class Quiz:
+    def __init__(self):
+        self.questions = []
+        self.currentQuestionIndex = -1
+
+    def startQuiz(self, player):
+        self.player = player
+        self.currentQuestionIndex = 0
+
+    def getNextQuestion(self):
+        if self.currentQuestionIndex < len(self.questions):
+            return self.questions[self.currentQuestionIndex]
+        else:
+            return None
+
+    def checkAnswer(self, answer):
+        current_question = self.questions[self.currentQuestionIndex]
+        if current_question.correctAnswer == answer:
+            return True
+        else:
+            return False
+
+    def updateScore(self, player):
+        if self.checkAnswer:
+            player.score += 1
