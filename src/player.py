794f0c8d9c57e856e7d63112d8c3b6b82f54e220
zharfan-a <zharfan@walturn.com>
2024-09-18 14:39:30 +0700

    Add src/player.py

---
+++ src/player.py
@@ -0,0 +1,15 @@
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+    def update_score(self, score):
+        if not isinstance(score, int):
+            raise TypeError("Score must be an integer")
+        if score < 0:
+            raise ValueError("Score cannot be negative")
+        self.score += score
+
+    def reset_score(self):
+        self.score = 0
